d302e9283dd93d1b713f0cc645137d9bb7010169
Eduardo Verdeja <eaverdeja@gmail.com>
2018-08-08 20:15:02 -0300

    213. Navigating to the Contact Data Component

--- my-burger/src/containers/Checkout/Checkout.js
+++ my-burger/src/containers/Checkout/Checkout.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import qs from 'query-string'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
     state = {
@@ -38,6 +40,7 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients} 
                     checkoutCanceled={this.checkoutCanceledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
             </div>
         ) 
     }
