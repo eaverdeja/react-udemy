e36dd93bc0b15c63f44b3113519febf5eed18d3d
Eduardo Verdeja <eaverdeja@gmail.com>
2018-08-08 19:59:35 -0300

    211. Navigating Back & To Next Page

--- my-burger/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ my-burger/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -5,14 +5,17 @@ import Button from '../../UI/Button/Button'
 import classes from './CheckoutSummary.css'
 
 const checkoutSummary = (props) => {
-    
     return (
         <div className={classes.CheckoutSummary} >
             <h1>We hope it tastes good!</h1>
-            <div style={{width: '100%', height: '300px', margin: 'auto'}}>
+            <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients} />
-                <Button btnType="Danger">CANCEL</Button>
-                <Button btnType="Success">CONTINUE</Button>
+                <Button
+                    btnType="Danger"
+                    clicked={props.checkoutCanceled}>CANCEL</Button>
+                <Button
+                    btnType="Success"
+                    clicked={props.checkoutContinued}>CONTINUE</Button>
             </div>
         </div>
     )
--- my-burger/src/containers/Checkout/Checkout.js
+++ my-burger/src/containers/Checkout/Checkout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 
 class Checkout extends Component {
@@ -11,12 +12,23 @@ class Checkout extends Component {
         }
     }
 
+    checkoutCanceledHandler = () => {
+        this.props.history.goBack()
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.push('/checkout/contact-data')
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients} 
+                    checkoutCanceled={this.checkoutCanceledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler} />
             </div>
-        )
+        ) 
     }
 }
 
