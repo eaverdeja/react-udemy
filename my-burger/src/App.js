487ba013b14e33ff5ff175f38b73ccf0f6924b9b
Eduardo Verdeja <eaverdeja@gmail.com>
2018-08-08 21:45:44 -0300

    215. Adding an Orders Page

--- my-burger/src/App.js
+++ my-burger/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout';
+import Checkout from './containers/Checkout/Checkout'
+import Orders from './containers/Orders/Orders'
 
 class App extends Component {
   render() {
@@ -12,10 +13,11 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder}></Route>
           <Route path="/checkout" component={Checkout}></Route>
+          <Route path="/orders" component={Orders}></Route>
         </Layout>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
