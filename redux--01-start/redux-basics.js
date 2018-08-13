1fdee58514fd5768f81b32663052583f4635235f
Eduardo Verdeja <eaverdeja@gmail.com>
2018-08-13 20:31:22 -0300

    243. Dispatching Actions

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -4,7 +4,27 @@ const initialState = {
     counter: 0
 }
 
-const rootReducer = (state = initialState, action) => state
+const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        default:
+            return state
+    }
+    state
+}
 
 const store = redux.createStore(rootReducer)
+console.log(store.getState())
+
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'ADD', value: 10})
 console.log(store.getState())
