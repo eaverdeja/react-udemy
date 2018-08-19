f23173404cd466d994686e479b8dd1442a879616
Eduardo Verdeja <eaverdeja@gmail.com>
2018-08-19 13:35:23 -0300

    262. Finishing the Reducer for Ingredients

--- my-burger/src/index.js
+++ my-burger/src/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter } from 'react-router-dom'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { SET_INGREDIENTS, ADD_INGREDIENT, REMOVE_INGREDIENT } from './store/actions'
 import rootReducer from './store/rootReducer';
+
 const store = createStore(rootReducer);
+
+/**
+ * Our actions get dispatched to the reducer,
+ * which updates the state just fine :)
+ * In the next commits we will use react-redux's
+ * connect() to inject the ingredients and actions
+ * as props to the BurgerBuilder container
+ * The code block below will be removed, together
+ * with lot's of BurgerBuilder code
+ */
+store.dispatch({type: SET_INGREDIENTS})
+console.log(store.getState())
+store.dispatch({type: ADD_INGREDIENT, ingredientType: 'salad'})
+console.log(store.getState())
+store.dispatch({type: REMOVE_INGREDIENT, ingredientType: 'bacon'})
+console.log(store.getState())
 
 const app = (
     <Provider store={store}>
--- my-burger/src/store/reducers/burger.js
+++ my-burger/src/store/reducers/burger.js
@@ -1,11 +1,49 @@
-import { ADD_INGREDIENT, REMOVE_INGREDIENT } from "../actions";
+import { ADD_INGREDIENT, REMOVE_INGREDIENT, SET_INGREDIENTS } from "../actions";
 
-const initialState = {}
+const initialState = {
+    ingredients: null
+}
+
+function updateIngredient (state, ingredientType, op) {
+    const oldCount = state.ingredients[ingredientType]
+    
+    const newCount = op(oldCount, 1)
+    if(newCount < 0) return
+
+    return newCount
+}
 
 const burgerReducer = (state = initialState, action) => {
+    let newCount = null
     switch(action.type) {
+        case SET_INGREDIENTS: 
+            return {
+                ...state,
+                ingredients: {
+                    salad: 1,
+                    meat: 1,
+                    bacon: 2,
+                    cheese: 1
+                }
+            }
         case ADD_INGREDIENT:
+            newCount = updateIngredient(state, action.ingredientType, (a, b) => a + b)
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientType]: newCount
+                }
+            }
         case REMOVE_INGREDIENT:
+            newCount = updateIngredient(state, action.ingredientType, (a, b) => a - b)
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientType]: newCount
+                }
+            }
         default:
             return state
     }
