0ca59031ff9ae61b699bd85fa19124a3033ab24c
Eduardo Verdeja <eaverdeja@gmail.com>
2018-08-26 01:19:01 -0300

    279. Using Utility Functions

--- redux--01-start/src/store/reducers/counter.js
+++ redux--01-start/src/store/reducers/counter.js
@@ -4,6 +4,7 @@ import {
     ADD,
     SUBTRACT
 } from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
     value: 0
@@ -12,25 +13,13 @@ const initialState = {
 const counter = (state = initialState, action) => {
     switch(action.type) {
         case INCREMENT:
-            return {
-                ...state,
-                value: state.value + 1
-            }
+            return updateObject(state, { value: state.value + 1 })
         case DECREMENT:
-            return {
-                ...state,
-                value: state.value - 1
-            }
+            return updateObject(state, { value: state.value - 1 })
         case ADD:
-            return {
-                ...state,
-                value: state.value + action.value
-            }
+            return updateObject(state, { value: state.value + action.value })
         case SUBTRACT:
-            return {
-                ...state,
-                value: state.value - action.value
-            }
+            return updateObject(state, { value: state.value - action.value })
         default:
             return state
     }
